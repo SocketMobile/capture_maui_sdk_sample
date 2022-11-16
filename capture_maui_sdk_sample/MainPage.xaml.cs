49da20b8415f75289b347491218c5e34f4eebf0c
Matt Robbins <matt.robbins@socketmobile.com>
2022-11-16 13:23:37 -0600

    Add all supported platforms

--- capture_maui_sdk_sample/MainPage.xaml.cs
+++ capture_maui_sdk_sample/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Maui;
 using SocketMobile.Capture;
+using System.ComponentModel;
+using System.Data;
 using System.Text.RegularExpressions;
 
 namespace capture_maui_sdk_sample;
@@ -21,20 +23,19 @@ public partial class MainPage : ContentPage
         {
             appId = "ios:com.socketmobile.capturemauisdksample";
             developerId = "ec9e1b16-c5ae-ec11-983e-000d3a5bbc61";
-            appKey = "MC0CFQCmP6oKJtSK5RXgIpPBUhXsjg63rwIUbS8tmz3f2JpdoNjfhHWGmjv5+AI=";
-
+            appKey = "MCwCFEkb64A52C9k5eFixm5cxROCIAvuAhQEdRLDDwWm0118ZQ2Wanm3+mTsWw==";
         }
         if (DeviceInfo.Platform == DevicePlatform.Android)
         {
-            appId = "android:com.socketmobile.multiplatformtest.android";
+            appId = "android:com.socketmobile.capturemauisdksample";
             developerId = "ec9e1b16-c5ae-ec11-983e-000d3a5bbc61";
-            appKey = "MCwCFBOpqbd3TEZivPYJ2oKHKASgPD+yAhRVLLyX/+yQpa3LXAMC+MpgiwdXsA==";
+            appKey = "MC0CFAHlouPuU92TWdlHoQR6Fouwo/ldAhUA7JTa66UIqUfCS3NFFbBhd6VyMQ4=";
         }
         if (DeviceInfo.Platform == DevicePlatform.WinUI)
         {
-            appId = "windows:com.socketmobile.multiplatformtest.windows";
+            appId = "windows:com.socketmobile.capturemauisdksample";
             developerId = "ec9e1b16-c5ae-ec11-983e-000d3a5bbc61";
-            appKey = "MCwCFAc20rtTcO+lDlodlK/+LVvx+plcAhQpDPwo3SyqMWdm4NbUBWAd5IRv/A==";
+            appKey = "MCwCFHujtHDn4yJI2YV0qeOjDhQ1D7cdAhR9JoCEtiO56BX8DeL6tcZwQW2j0w==";
         }
 
         capture.OpenAsync(appId, developerId, appKey)
@@ -52,26 +53,28 @@ public partial class MainPage : ContentPage
 
     private void Capture_DecodedData(object sender, CaptureHelper.DecodedDataArgs e)
     {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            DataLabel.Text += "\n" + $"[{DateTime.Now}] " + "Decoded Data: " + System.Text.Encoding.UTF8.GetString(e.DecodedData.Data);
+        });
     }
 
     private void Capture_DeviceRemoval(object sender, CaptureHelper.DeviceArgs e)
     {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            DataLabel.Text += "\n" + $"[{DateTime.Now}]" + " Device Removal" + "\n";
+        });
     }
 
-    private void Capture_DeviceArrival(object sender, CaptureHelper.DeviceArgs e)
+    private async void Capture_DeviceArrival(object sender, CaptureHelper.DeviceArgs e)
     {
+        MainThread.BeginInvokeOnMainThread(async () =>
+        {
+            DataLabel.Text += $"[{DateTime.Now}] " + "Device Arrival";
+            var name = await e.CaptureDevice.GetFriendlyNameAsync();
+            DataLabel.Text += "\n" + $"[{DateTime.Now}] " + "Friendly Name: " + name.FriendlyName;
+        });
     }
-
-    private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
--- capture_maui_sdk_sample/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace capture_maui_sdk_sample;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
